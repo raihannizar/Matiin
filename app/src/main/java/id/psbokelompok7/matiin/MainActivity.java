9d75fedab9a29d81940613c4828a3bbe3db4c7cd
rizkyfernanda <mrizkyfer@gmail.com>
2019-05-15 13:47:11 +0700

    Tambah Splash Screen

--- app/src/main/java/id/psbokelompok7/matiin/MainActivity.java
+++ app/src/main/java/id/psbokelompok7/matiin/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         loadStateCardWaktu();   //Load keadaan terbaru.
         buildCardWaktu();
         setButtonTambahWaktuAlarm();
---
+++ app/src/main/java/id/psbokelompok7/matiin/Splash.java
@@ -0,0 +1,32 @@
+package id.psbokelompok7.matiin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //hiding the status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, 2500);
+    }
+}
